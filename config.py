fd336409ebd345b9011f055340e8b5daac03683e
yeshsgit <166917663+yeshsgit@users.noreply.github.com>
2024-07-19 16:56:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-file_path = ""
-file_name = ""
-file_url = ""
-chunk_size = int
-llm_model = ""
-embedding_model = ""
-filtered_chunks_file_path = ""
+file_path = "" # where and what you want to save the pdf as
+pdf_name = "" # name of the pdf
+file_url = "" # url to the pdf
+chunk_size = int # number of sentences that will be embedded in one chunk, take into consideration the embedding model's token limit
+llm_model = "" # name of llm
+embedding_model = "" # name of embedding model
+filtered_chunks_file_path = "" # where you want to save chunks that have gone through preprocessing to avoid repeating
